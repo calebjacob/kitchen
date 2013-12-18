230121a4e6212b09d8ca0b99b1fddc30a3015ca2
iamceege <hello@calebjacob.com>
2013-12-18 08:44:13 -0700

    Getting started with a basic Node server

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Partay!');
+});
+
+app.listen(3000);
+console.log('There\'s a party going on over at :3000');
